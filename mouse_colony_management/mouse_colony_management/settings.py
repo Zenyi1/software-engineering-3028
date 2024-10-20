03e71a4af45fb01c520fc119749175d7b83b675e
gonz_tyler <gonzaloescolartyler@gmail.com>
2024-10-20 17:13:16 +0100

    Update settings.py

--- mouse_colony_management/mouse_colony_management/settings.py
+++ mouse_colony_management/mouse_colony_management/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import environ
+import environ # environ module allows the use of .env file to store database credentials
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
